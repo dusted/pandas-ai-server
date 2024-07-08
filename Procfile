b83ed8861bb9b30503e11e51afc71d9c0290a185
Jamie Schaedel <jamie.schaedel@dusted.com>
2024-07-09 00:05:52 +0100

    Revert "Proc"
    This reverts commit d07985fb8203737e0b758d3b0b01ebe1cb7b9b6f.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: uvicorn core.server:app --host 0.0.0.0 --port $PORT --workers 1
--- core/config.py
+++ core/config.py
@@ -1,10 +1,5 @@
 from enum import Enum
-from pydantic import BaseSettings
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file if not already loaded
-load_dotenv()
+from pydantic import BaseSettings, PostgresDsn
 
 class EnvironmentType(str, Enum):
     DEVELOPMENT = "development"
@@ -19,17 +14,17 @@ class Config(BaseConfig):
     DEBUG: int = 0
     DEFAULT_LOCALE: str = "en_US"
     ENVIRONMENT: str = EnvironmentType.DEVELOPMENT
-    POSTGRES_URL: str
-    CHAT_DB_URL: str
+    POSTGRES_URL: PostgresDsn
+    CHAT_DB_URL: PostgresDsn
     OPENAI_API_KEY: str = None
     RELEASE_VERSION: str = "0.1.0"
     SHOW_SQL_ALCHEMY_QUERIES: int = 0
     SECRET_KEY: str = "super-secret-key"
     JWT_ALGORITHM: str = "HS256"
     JWT_EXPIRE_MINUTES: int = 60 * 24
-    EMAIL: str = "test@pandabi.ai"
-    PASSWORD: str = "12345"
-    DEFAULT_ORGANIZATION: str = "PandaBI"
-    DEFAULT_SPACE: str = "pandasai"
+    EMAIL = "test@pandabi.ai"
+    PASSWORD = "12345"
+    DEFAULT_ORGANIZATION = "PandaBI"
+    DEFAULT_SPACE = "pandasai"
 
 config = Config()
