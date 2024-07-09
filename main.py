76f6eb146708d8678a3a5d38c923d2a8a64be04c
Jamie Schaedel <jamie.schaedel@dusted.com>
2024-07-09 20:28:00 +0100

    port for heroku

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ load_dotenv()
 from core.config import config
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         app="core.server:app",
         reload=True if config.ENVIRONMENT != "production" else False,
         host=os.environ.get("SERVER_HOST", "0.0.0.0"),
-        port=os.environ.get("SERVER_PORT", 8000),
+        port=port,
         workers=1,
     )
